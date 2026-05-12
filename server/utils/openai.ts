3a8fd6abbc67437ddc5a7a3edc6c0611e5d85e39
BlackAdi <odai.shalabi@gmail.com>
2026-05-12 09:51:20 +0300

    fix env

--- server/utils/openai.ts
+++ server/utils/openai.ts
@@ -1,12 +1,11 @@
 import OpenAI from "openai";
 
-export const useOpenAI = () => {
-    const config = useRuntimeConfig()
+export const useOpenAI = defineEventHandler((event) => {
+    const config = useRuntimeConfig(event)
 
     return new OpenAI({
         apiKey: config.openaiKey
     });
-
-}
+})
 
 export const assistant = "asst_PNa2ailIBfh4Hv0JyJA10wrD"
